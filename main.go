ddca22d116572873d971ebdaccc95930c0d24524
David Bayless <David.C.Bayless15@gmail.com>
2017-02-10 10:36:17 -0700

    initial game compile

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"engo.io/ecs"
+	"engo.io/engo"
+)
+
+type myScene struct{}
+
+func (*myScene) Type() string { return "myGame" }
+
+func (*myScene) Preload() {}
+
+func (*myScene) Setup(*ecs.World) {}
 
 func main() {
+	opts := engo.RunOptions{
+		Title:  "Hello World",
+		Width:  800,
+		Height: 800,
+	}
 	fmt.Println("Hello!")
+	engo.Run(opts, &myScene{})
 }
